9bf698bc5d7d7c3b4fd660f2037b51bebd3bc2a6
Srinivasan <kksrini89@gmail.com>
2018-06-28 16:06:04 +0530

    Removing  web client id

--- src/providers/auth.service.ts
+++ src/providers/auth.service.ts
@@ -71,7 +71,7 @@ export class AuthProvider {
   async nativeGoogleLogin() {
 
     const gplusUser = await this.gplus.login({
-      'webClientId': '698468554914-mr69c77ffau1uf5vq3mg7arf2ekhv86m.apps.googleusercontent.com',
+      'webClientId': '', // Removed since security
       'offline': true,
       'scopes': 'profile email'
     })
@@ -80,7 +80,7 @@ export class AuthProvider {
     // try {
     // this.gplus
     //   .login({
-    //     'webClientId': '698468554914-mr69c77ffau1uf5vq3mg7arf2ekhv86m.apps.googleusercontent.com',
+    //     'webClientId': '',
     //     'offline': true,
     //     'scopes': 'profile email'
     //   })
