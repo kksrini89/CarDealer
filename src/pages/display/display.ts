657fea3ec381ad5812c4f31e58fddec88628eea5
kksrinivasan89 <kksrini89@gmail.com>
2018-07-03 13:34:17 +0530

    search bar added

--- src/pages/display/display.ts
+++ src/pages/display/display.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-import { CarAdminProvider } from '../../providers/car-admin.service';
-import { Car } from '../../models/car.model';
 import { Observable } from 'rxjs';
+
+import { Car } from '../../models/car.model';
+import { CarAdminProvider } from '../../providers/car-admin.service';
 
 @IonicPage()
 @Component({
@@ -11,7 +11,10 @@ import { Observable } from 'rxjs';
   templateUrl: 'display.html'
 })
 export class DisplayPage {
+  searchInput: string = '';
+
   carDealsList$: Observable<any[]>;
+  carList: any[];
 
   constructor(
     public navCtrl: NavController,
@@ -21,10 +24,36 @@ export class DisplayPage {
 
   ionViewWillEnter() {
     this.carDealsList$ = this.carProvider.getCars();
+    this.carDealsList$.subscribe(items => (this.carList = items));
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DisplayPage');
+  }
+
+  OnInput(event: any) {
+    let tempSpace = [];
+    this.carProvider.getCars().subscribe(items => {
+      tempSpace = items;
+      let inputValue = event.target.value;
+      if (inputValue && inputValue.trim() !== '') {
+        this.carList =
+          tempSpace &&
+          tempSpace.length &&
+          tempSpace.filter((item: any) => {
+            return (
+              item.model
+                .toString()
+                .toLowerCase()
+                .indexOf(inputValue.toLowerCase()) > -1
+            );
+          });
+      }
+    });
+  }
+
+  OnCancel(event: any) {
+    this.carProvider.getCars().subscribe(items => (this.carList = items));
   }
 
   getDetail(car: Car) {
